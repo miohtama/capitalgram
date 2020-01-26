05171bfe75573598577c62ed45d3777bc8e99317
Mikko Ohtamaa <mikko@opensourcehacker.com>
2020-01-26 09:05:21 +0100

    Adding MailerLite client

---
+++ src/utils/mailerlite.js
@@ -0,0 +1,100 @@
+// Simplified MailerLite API client
+
+const axios = require('axios');  // https://www.npmjs.com/package/axios
+
+
+/**
+ * Quick wrapper around mailerlite
+ * 
+ * https://developers.mailerlite.com/docs/request
+ * 
+ */
+ class MailerLiteClient {
+
+    constructor(apiKey, endpoint=null) {
+        this.apiKey = apikey;
+
+        // Default API endpoint
+        if(!endpoint) {
+            endpoint = "https://api.mailerlite.com/api/v2";
+        }
+
+        this.endpoint = endpoint;
+    }
+
+    /**
+     * Makke a MailerLite API request
+     * 
+     * @param {*} path 
+     * @param {*} method 
+     * @param {*} params 
+     */
+    makeRequest(path, method="get", params={}) {
+        const fullPath = this.endpoint + path;
+
+        const headers = {
+            "X-MailerLite-ApiKey": this.apiKey
+        }
+
+        // https://developers.mailerlite.com/docs/response
+        try {
+            const resp = await axios.request({
+                url: fullPath,
+                method,
+                headers, 
+                data: params,
+            });
+    
+        } catch(e) {
+            // https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
+            const data = resp.data;
+            const message = data.message;
+            throw new MailerLiteError(message, e);
+        }
+
+        return resp;
+    }
+
+    subscribe(email, name=null) {
+
+        let payload = { email: email };
+
+        // Set full name
+        if(name) {
+            payload.name = name;
+        }
+
+        this.makeRequest("post", "/subscribers", payload);    
+    }
+ }
+
+
+ class MailerLiteError extends Error {    
+    constructor(message, exception) {
+        this.message = message;
+        this.exception = exception;
+    }
+}
+
+ /** 
+  * Some random tests gainst MailerLite production API to see how it behves.
+  * 
+  * To run: 
+  * 
+  *     MAILER_LITE_API_KEY = "..." node -e "require('src/utils/mailerlite.js').test()"
+  * 
+  * 
+  */
+ function test() {
+    const apiKey =  process.env.MAILER_LITE_API_KEY;
+    const client = MailerLiteClient(apiKey);
+
+    let resp = client.subscribe("dummy@example.com"); // https://en.wikipedia.org/wiki/Example.com
+    console.log(resp);
+ }
+
+
+ module exports = {
+    MailerLiteClient: MailerLiteClient
+    test: test,
+ };
