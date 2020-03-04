d401c1476f1c43a104882149feb828b5ab7b04ba
Mikko Ohtamaa <mikko@opensourcehacker.com>
2020-03-04 21:45:22 +0100

    Fix setting of base_url

--- src/config/social.js
+++ src/config/social.js
@@ -1,29 +1,29 @@
 /**
  * Data file for social media sharing titles and logos configuration.
- * 
+ *
  * Supported
  * - Facebook
  * - Twitter
- * 
+ *
  * See and edit social_media_head.njk to allow individual posts and pages to override these values,
  * e.g. for the page specific title image.
- * 
+ *
  * To use various Share debugggers to display local dev server pages.
  * Use https://pagekite.net/to create a tunnel from localhost.
  * For npx netlify run in localhost:8888 this would be
- * 
- * python2 pagekite.py 8888 mybloguniqueid.pagekite.me 
- * 
+ *
+ * python2 pagekite.py 8888 mybloguniqueid.pagekite.me
+ *
  * This config file is in Eleventy JS data file format: https://www.11ty.dev/docs/data-js/
  */
 
 module.exports = function() {
-    
+
     const general = {
 
         name: "Capitalgram",
 
-        // This is the path 
+        // This is the path
         // We give 512 x 512 square logo as the default logo if the pages do not supply their own
         default_logo: "/static/img/capitalgram-logo-square.png",
 
@@ -32,7 +32,7 @@ module.exports = function() {
         default_logo_height: 512,
 
         // Needed by Facebook sharing images, others... cannot have relative URLs
-        base_url: "https://capitalgram/com"
+        site_base_url: "https://capitalgram.com"
     };
 
     // Facebook Sharing debugger is available to preview your sharings
@@ -42,9 +42,9 @@ module.exports = function() {
     const facebook = {
 
         name: general.name,
-                
+
         // If your Facebook has a page, you get its id here
-        // 
+        //
         app_id: undefined,
 
         // Copy default logo information if the page does not override
@@ -52,7 +52,7 @@ module.exports = function() {
         logo: "/static/img/capitalgram-logo-facebook.png", // or general.default_logo,
         logo_width: 1200, // or general.default_logo_width,
         logo_height: 630, // or general.default_logo_height,
-        
+
         // One of https://ogp.me/?fbclid=IwAR0BAwLqqduH9pGKDGgLj7dpmokfXgVXcnEw0M6A4rTillc0q4L0qYpCJBk#types
         // We default to article
         type: "article",
